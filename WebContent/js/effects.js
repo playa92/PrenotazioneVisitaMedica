a59495677920aa6af5fe8b651a9c917e125b4ca7
Aloia <Aloia@Aloia-PC>
2018-01-10 23:21:25 +0100

    -dropdown -login(da finire)

--- WebContent/js/effects.js
+++ WebContent/js/effects.js
@@ -1,3 +1,4 @@
+// dropmenu 
 $(document).ready(function(){
 	
 	$('ol.nav li.dropdown').hover(
@@ -6,5 +7,53 @@ $(document).ready(function(){
 	}, function() {
   		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 	})	
+	
 });
+    
+$(document).ready(function(){
+	  $(this).find('ol.nav li.dropdown #navbar-text').click(
+	  function(){
+	      $(this).css('background-color', '#0066ff');
+	  }) 
+});
+
+$(document).ready(function(){
+
+	  $(this).find('ol.nav li.dropdown #navbar-text').mouseleave(
+	  function(){    
+	      $(this).css('background-color', '#0099ff');
+	  })
+});
+
+//login
+$(document).ready(function(){
+	    
+		$('input[type="submit"]').mousedown(function(){
+		  $(this).css('background', '#2ecc71');
+		});
+		$('input[type="submit"]').mouseup(function(){
+		  $(this).css('background', '#1abc9c');
+		});
+
+		$('#loginform').click(function(){
+		  $('.login').fadeToggle('slow');
+		  $(this).toggleClass('green');
+		})
+			
+		$(document).mouseup(function (e)
+		{
+		    var container = $(".login");
+
+		    if (!container.is(e.target) // if the target of the click isn't the container...
+		        && container.has(e.target).length === 0) // ... nor a descendant of the container
+		    {
+		        container.hide();
+		        $('#loginform').removeClass('green');
+		    }
+		})
+	
+	});
+
+
+
 
