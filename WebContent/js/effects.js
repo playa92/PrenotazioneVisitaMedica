e62b20d3988b6cdfcfe7477aa6b4b47768f97a3b
Aloia <Aloia@Aloia-PC.homenet.telecomitalia.it>
2018-01-12 20:59:03 +0100

    risolto Login click

--- WebContent/js/effects.js
+++ WebContent/js/effects.js
@@ -1,5 +1,5 @@
-var v = 0; //visibilità file
 
+/* NAVBAR LEFT ELEMENTS*/
 $(document).ready(function() {
 	
 	$("ol.nav li.dropdown").focusout(
@@ -9,7 +9,7 @@ $(document).ready(function() {
 	
 	$('ol.nav li.dropdown').click(
 	function() {
-		$(this).children('.dropdown-menu').slideDown('fast'),
+		$(this).children('.dropdown-menu').slideToggle('fast'),
         $(this).toggleClass("dropdown-active");
 	});
 	
@@ -22,25 +22,36 @@ $(document).ready(function() {
 	function() {    
 	   $(this).css('background-color', '#0099ff');
 	})
+	
+});
 
-	//navbar login
+/* NAVBAR LOGIN */
+$(document).ready(function() {
+	
 	$(this).find('ul.nav li.dropdown a#login-form').click(
 	function() {    
 	   $(this).css('background-color', '#0066ff');
 	})
- 
+	
 	$(this).find('ul.nav li.dropdown a#login-form').mouseleave(
 	function() {    
 	   $(this).css('background-color', '#0099ff');
 	})
-
-	//login form
-	$('#login-form').click(function() {
-	  $('.login').fadeToggle('slow');
+	
+	$("#login-form").click(function(e){
+		$(".login").show();
+		e.stopPropagation();
 	});
-
+	
+	$(".login").click(function(e){
+		e.stopPropagation();
+	});
+});
+$(document).click(function(){ // fa parte del NAVBAR LOGIN ma deve stare cosi
+	$(".login").hide();
 });
 
+/* PRELOADER */
 $(window).on('load', function () {
 	 // executes when complete page is fully loaded, including all frames, objects and images
 		$('#preloader').fadeOut('slow',function(){$(this).remove();});
@@ -50,23 +61,19 @@ function pulisci() {
 	alert("Attenzione! I dati inseriti verrano puliti");
 }
 
+var v = 0; //visibilità file
 function controllo() {
-	
 	$('#form input').each(		
 		function(index) {  			
-			
 			var input = $(this);
 			var a = String(input.attr('type'));
 				
-			if(a == "text") {
-				
+			if(a == "text") {		
 				if(input.val() == "") {				
-					
 //					alert("before " + input.attr('style'));
 //					input.attr({'style': 'border:1px solid red'});
 //					alert("after " + input.attr('style'));
 					alert("Attenzione! Alcuni campi sono vuoti");
-						
 //					return false;
 				}
 			}		
