6cf42aae209aa47050a572c067d8c129ca3dad10
giocos <giovannicosentino1994@gmail.com>
2018-01-13 11:12:41 +0100

    -aggiunto bordo rosso se durante la prenotazione i campi sono vuoti(SI NOTA SOLO SU FIREFOX). -alternato il colore del click sulle navbar Statistiche e Assistenza, ora si vede blu scuro e quando clicchi su un altro text viene tolto correttamente. -quando le navbar vengono chiuse non è presente lo slide(DA RICONTROLLARE).

--- WebContent/js/effects.js
+++ WebContent/js/effects.js
@@ -1,18 +1,53 @@
-
 /* NAVBAR LEFT ELEMENTS*/
+var click_l = 0, click_r = 0; //visiblità file
 $(document).ready(function() {
 		
 	$('ol.nav li.dropdown').click(
-		function() {
+		function(e) {
 			$(this).children('.dropdown-menu').slideToggle('fast'),
 	        $(this).toggleClass("dropdown-active");
+			
+			//TODO GIOVANNI: bisogna semplificarlo
+			if(e.target.id == 'slide-navbar-l') {
+				
+				if(click_l == 0) {
+					click_l = 1;
+					$(this).find('#slide-navbar-l').css('background-color', '#0066ff');
+				}
+				else {
+					click_l = 0;
+					$(this).find('#slide-navbar-l').css('background-color', '#0099ff');
+				}
+				return;
+			}
+			
+			if(e.target.id == 'slide-navbar-r') {
+				
+				if(click_r == 0) {
+					click_r = 1;
+					$(this).find('#slide-navbar-r').css('background-color', '#0066ff');
+				}
+				else {
+					click_r = 0;
+					$(this).find('#slide-navbar-r').css('background-color', '#0099ff');
+				}
+				return;
+			}
 			$(this).find('#navbar-text').css('background-color', '#0066ff');
+			///////////////////////////////////////////////////////////////
 	});
 	
 	$("ol.nav li.dropdown").focusout(
 		function() {
 			$(this).children('.dropdown-menu').slideUp('fast');
+			$(this).children('.dropdown-menu').stop(true, true);
 			$(this).find('#navbar-text').css('background-color', '#0099ff');
+			$(this).find('#slide-navbar-l').css('background-color', '#0099ff');
+			$(this).find('#slide-navbar-r').css('background-color', '#0099ff');
+			
+			/* resetto i valori per gli eventi successivi */
+			click_l = 0;
+			click_r = 0;
 	});
 		
 });
@@ -20,23 +55,24 @@ $(document).ready(function() {
 /* NAVBAR LOGIN */
 $(document).ready(function() {
 	
-	$("#login-form").click(function(e){
+	$("#login-form").click(function(e) {
 		$(".login").show();
 		e.stopPropagation();
 		$(this).css('background-color', '#0066ff');
 	});
 	
-	$(".login").click(function(e){
+	$(".login").click(function(e) {
 		e.stopPropagation();
 	});
 });
-$(document).click(function(){ // fa parte del NAVBAR LOGIN ma deve stare cosi
+
+$(document).click(function() { // fa parte del NAVBAR LOGIN ma deve stare cosi
 	$(".login").hide();
 	$(this).find('ul.nav li.dropdown a#login-form').css('background-color', '#0099ff');
 });
 
 /* PRELOADER */
-$(window).on('load', function () {
+$(window).on('load', function() {
 	 // executes when complete page is fully loaded, including all frames, objects and images
 		$('#preloader').fadeOut('slow',function(){$(this).remove();});
 });
@@ -45,8 +81,9 @@ function pulisci() {
 	alert("Attenzione! I dati inseriti verrano puliti");
 }
 
-var v = 0; //visibilità file
+var entra = false;
 function controllo() {
+	
 	$('#form input').each(		
 		function(index) {  			
 			var input = $(this);
@@ -54,13 +91,13 @@ function controllo() {
 				
 			if(a == "text") {		
 				if(input.val() == "") {				
-//					alert("before " + input.attr('style'));
-//					input.attr({'style': 'border:1px solid red'});
-//					alert("after " + input.attr('style'));
-					alert("Attenzione! Alcuni campi sono vuoti");
-//					return false;
+					//TODO GIOVANNI: al momento funziona solo con FIREFOX
+					entra = true;
+					input.attr({'style': 'border:1px solid red'});
 				}
 			}		
 	});
-	return true;
+	
+	if(entra) 
+		alert("Attenzione! Alcuni campi sono vuoti");
 }
