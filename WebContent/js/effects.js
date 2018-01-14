004967e1e75e0a51c75120198741ddb570e7b731
giocos <giovannicosentino1994@gmail.com>
2018-01-14 13:21:57 +0100

    -configurato xml per eseguire la pagina su tomcat -il bordo rosso nel form della prenotazione ritorna bianco quando avviene un evento -aggiunto footer nella pagina prenotazione

--- WebContent/js/effects.js
+++ WebContent/js/effects.js
@@ -1,8 +1,7 @@
-
 /* NAVBAR LOGIN */
 $(document).ready(function() {
 	
-	$("#login-form").click(function(e){
+	$("#login-form").click(function(e) {
 		$(".login").fadeToggle();
 		e.stopPropagation();
 	});
@@ -11,36 +10,43 @@ $(document).ready(function() {
 		e.stopPropagation();
 	});
 });
+
 $(document).click(function(){
 	$(".login").fadeOut();
 });
 
 /* PRELOADER */
 $(window).on('load', function () {
-	 // executes when complete page is fully loaded, including all frames, objects and images
-		$('#preloader').fadeOut('slow',function(){$(this).remove();});
+	// executes when complete page is fully loaded, including all frames, objects and images
+	$('#preloader').fadeOut('slow',function(){$(this).remove();});
 });
 
 function pulisci() {
 	alert("Attenzione! I dati inseriti verrano puliti");
 }
 
-var entra = false;
 function controllo() {
 	
-	$('#form input').each(function(){
-	    if(!$(this).val().trim()){
-	        $(this).css({
-	        	"border":"1px solid red"
-	        })
+	var entra = false;
+	$('#form input').each(function() {
+	    if(!$(this).val().trim()) {
+	    	
+	        $(this).css({"border":"1px solid red"});
 	        entra=true;
-	    }else{
-	        $(this).css({
-	        	"border":"1px solid white"
-	    	})
+	    }else {
+	        $(this).css({"border":"1px solid white"});
 	    }
-	})
+	});
+	
 	if(entra)
-	alert("Attenzione! Alcuni campi sono vuoti");
+		alert("Attenzione! Alcuni campi sono vuoti");
 }
+
+$(document).ready(function() {
+    $("#form").click(function(e) {
+        if(e.target.style.border == "1px solid red") {
+        	$("#form input").css({"border":"1px solid white"});
+        }
+    });
+});
 
